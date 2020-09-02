461353aa875dd53fe6a7f461bb663b7b95c2ffbf
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-02 18:19:40 +0300

    3

--- main.js
+++ main.js
@@ -16,6 +16,37 @@ const btnId = document.querySelector('.btn-id');
 btnId.addEventListener('click', ()=>{
     idGenerator.next();
 });
+function* incline(){
+    const par = document.querySelector('.par');
+    let size = parseInt(par.style.fontSize);
+    for(let i = size; i < Infinity; i++){
+        yield i;
+        par.style.fontSize = i + 1 +'px';
+        console.log(par.style.fontSize);
+    }
+}
+const plusSize = incline();
+const plus = document.querySelector('.plus');
+plus.addEventListener('click', ()=>{
+    plusSize.next();
+})
+
+function* decline(){
+    const par = document.querySelector('.par');
+    let size = parseInt(par.style.fontSize);
+    for(let i = size; i > 0; i--){
+        yield i;
+        par.style.fontSize = i - 1 +'px';
+        console.log(par.style.fontSize);
+    }
+}
+const minSize = decline();
+const minus = document.querySelector('.minus');
+minus.addEventListener('click', ()=>{
+    minSize.next();
+})
+
+
 
 
 
